adbaf3834de850d8d0384114051a47e0a372c2ea
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2023-07-03 21:55:05 -0300

    Criando Componentes

--- projetinho/src/App.js
+++ projetinho/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWord from './components/HelloWord';
 
 function App() {
+
+  const name = 'Rodrigo';
+  const url = "https://via.placeholder.com/200";
+
+  function multi(a, b){
+    return a * b;
+  }
+
   return (
     <div className="App">
         <h1>
-          Teste.
+          Teste
         </h1>
+        <p>{name}</p>
+        <p>Soma: {2 + 8}</p>
+        <p>Multiplicação com função: {multi(3,9)}</p>
+        <img src={url} alt="minha imagem"/>
+        <HelloWord/>
     </div>
   );
 }
---
+++ projetinho/src/components/Frase.js
@@ -0,0 +1,7 @@
+function Frase(){
+    return(
+        <div>Este é um componente importado dentro de outro componente importado.</div>
+    )
+}
+
+export default Frase;
